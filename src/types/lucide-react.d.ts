955c8e8137fd32111e22113a734984ca28f2ec24
Rehan77 <rehanfreestyle77@gmail.com>
2026-07-08 15:44:00 +0000

    error fixed

---
+++ src/types/lucide-react.d.ts
@@ -0,0 +1,19 @@
+declare module 'lucide-react' {
+  import type { ComponentType, SVGProps } from 'react';
+
+  type LucideIcon = ComponentType<SVGProps<SVGSVGElement> & { size?: number | string }>;
+
+  export const Code2: LucideIcon;
+  export const Gamepad2: LucideIcon;
+  export const Smartphone: LucideIcon;
+  export const Menu: LucideIcon;
+  export const X: LucideIcon;
+  export const Download: LucideIcon;
+  export const ArrowRight: LucideIcon;
+  export const Github: LucideIcon;
+  export const Linkedin: LucideIcon;
+  export const Mail: LucideIcon;
+  export const Send: LucideIcon;
+  export const CheckCircle2: LucideIcon;
+  export const ExternalLink: LucideIcon;
+}
